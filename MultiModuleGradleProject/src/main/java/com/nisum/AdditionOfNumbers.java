172bd56e61d8b9750bbd37107ca553b12f9a6f13
ksgr999 <ksgr999@gmail.com>
2014-12-03 11:17:17 +0530

    Added overloaded method

---
+++ MultiModuleGradleProject/src/main/java/com/nisum/AdditionOfNumbers.java
@@ -0,0 +1,15 @@
+package com.nisum;
+
+public class AdditionOfNumbers{
+
+	public int add(int number1, int number2) {
+		int add = number1 + number2;
+		return add;
+	}
+	
+	public int add1(int number1, int number2, int number3) {
+		int add = number1 + number2 + number3;
+		return add;
+	}
+
+}
